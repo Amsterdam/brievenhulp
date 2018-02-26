55fa23cac68cdc3dd290b3cafacff93a05928dda
kelvinpaska <k.paska@amsterdam.nl>
2018-02-26 11:59:16 +0100

    docker-entrypoint en docker-compose aangepast

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,38 @@ echo Starting server
 set -u
 set -e
 
+DB_HOST=${SYMFONY__BRIEVENHULP__DATABASE_HOST:-brievenhulp-db.service.consul}
+DB_PORT=${SYMFONY__BRIEVENHULP__DATABASE_PORT:-5432}
+
+cat > /srv/web/brievenhulp/app/config/parameters.yml <<EOF
+parameters:
+   database_host: ${DB_HOST}
+   database_port: ${DB_PORT}
+   database_name: ${SYMFONY__BRIEVENHULP__DATABASE_NAME}
+   database_user: ${SYMFONY__BRIEVENHULP__DATABASE_USER}
+   database_password: ${SYMFONY__BRIEVENHULP__DATABASE_PASSWORD}
+   mailer_transport: ${SYMFONY__BRIEVENHULP__MAILER_TRANSPORT}
+   mailer_host: ${SYMFONY__BRIEVENHULP__MAILER_HOST}
+   mailer_user: ${SYMFONY__BRIEVENHULP__MAILER_USER}
+   mailer_password: ${SYMFONY__BRIEVENHULP__MAILER_PASSWORD}
+   mailer_port: ${SYMFONY__BRIEVENHULP__MAILER_PORT}
+   mailer_encryption: ${SYMFONY__BRIEVENHULP__MAILER_ENCRYPTION}
+   mail_from: ${SYMFONY__BRIEVENHULP__MAIL_FROM}
+   mail_cc: ${SYMFONY__BRIEVENHULP__MAIL_CC}
+   auto_login_from_email: ${SYMFONY__BRIEVENHULP__AUTO_LOGIN_FROM_EMAIL}
+   retention_policy: ${SYMFONY__BRIEVENHULP__RETENTION_POLICY}
+   secret: ${SYMFONY__BRIEVENHULP__SECRET}
+   messagebird_accountkey: ${SYMFONY__BRIEVENHULP__MESSAGEBIRD_API_KEY}
+   messagebird_enable: ${SYMFONY__BRIEVENHULP__MESSAGEBIRD_ENABLE}
+   sms_originator: ${SYMFONY__BRIEVENHULP__SMS_ORGINATOR}
+   sms_disable: false
+   trusted_proxies:
+        - 127.0.0.1
+        - 10.0.0.0/8
+        - 172.16.0.0/12
+        - 192.168.0.0/16
+EOF
+
 # Already in Dockerfile (?)
 php composer.phar install -d brievenhulp/
 
